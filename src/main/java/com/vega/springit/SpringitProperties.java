fad4391abbf2911b8451bc0fff0152a5d1bc9ab8
brian@inetoc.com <DXt8E0s9wRObTCp6Vf8U>
2021-01-07 12:28:26 -0800

    Working on model and setting it up.

---
+++ src/main/java/com/vega/springit/SpringitProperties.java
@@ -0,0 +1,25 @@
+package com.vega.springit;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties(prefix = "springit")
+public class SpringitProperties {
+
+    /**
+     * This is our welcome message
+     *
+     */
+    private String welcomeMsg = "I love Jesus!";
+
+    public String getWelcomeMsg() {
+        return welcomeMsg;
+    }
+
+    public void setWelcomeMsg(String welcomeMsg) {
+        this.welcomeMsg = welcomeMsg;
+    }
+}
