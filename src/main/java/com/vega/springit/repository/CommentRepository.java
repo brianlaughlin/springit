a9dbb3595cc70e3f99972450f306ada789e3a2d6
brian@inetoc.com <DXt8E0s9wRObTCp6Vf8U>
2021-01-09 15:02:26 -0800

    updated appliaction properties and added h2

--- src/main/java/com/vega/springit/repository/CommentRepository.java
+++ src/main/java/com/vega/springit/repository/CommentRepository.java
@@ -1,8 +1,8 @@
 package com.vega.springit.repository;
 
+import com.vega.springit.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.xml.stream.events.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
